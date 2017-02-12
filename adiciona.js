8ac99803a730e46df22e856c2bf828b3f70f7437
juniorsolo <junior.solo@hotmail.com>
2017-02-11 21:40:32 -0300

    Pegando tabela pelo querySelector.

---
+++ adiciona.js
@@ -0,0 +1,13 @@
+var pacienteNovo = "<tr class='paciente'><td class='info-nome'>Rafael</td>"+
+						"<td class='info-peso'>90</td><td class='info-altura'>1.65</td>"+
+						"<td class='info-imc'></td></tr>";
+
+// var tabela = document.getElementsByTagName("table")[0];
+
+// retorna um array
+//document.querySelectorAll("table");
+
+//sempre pega o primeiro elemento de varios se houver
+var tabela  = document.querySelector("table");
+
+tabela.innerHTML = tabela.innerHTML + pacienteNovo;
