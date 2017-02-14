fca0612731c4c802344d6b3f39e3c6e6d29ac617
juniorsolo <junior.solo@hotmail.com>
2017-02-14 20:32:10 -0300

    Adiconando evento ao botão com event.preventDefault

--- adiciona.js
+++ adiciona.js
@@ -1,13 +1,20 @@
-var pacienteNovo = "<tr class='paciente'><td class='info-nome'>Rafael</td>"+
+var botaoAdd = document.querySelector("#adicionar-paciente");
+
+botaoAdd.addEventListener("click", function(event){
+
+	event.preventDefault();
+
+	var pacienteNovo = "<tr class='paciente'><td class='info-nome'>Rafael</td>"+
 						"<td class='info-peso'>90</td><td class='info-altura'>1.65</td>"+
 						"<td class='info-imc'></td></tr>";
 
-// var tabela = document.getElementsByTagName("table")[0];
+	// var tabela = document.getElementsByTagName("table")[0];
 
-// retorna um array
-//document.querySelectorAll("table");
+	// retorna um array
+	//document.querySelectorAll("table");
 
-//sempre pega o primeiro elemento de varios se houver
-var tabela  = document.querySelector("table");
+	//sempre pega o primeiro elemento de varios se houver
+	var tabela  = document.querySelector("table");
+	tabela.innerHTML = tabela.innerHTML + pacienteNovo;
 
-tabela.innerHTML = tabela.innerHTML + pacienteNovo;
+})
