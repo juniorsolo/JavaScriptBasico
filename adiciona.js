11ed8335d53d481d549978e1f72bb44420ec5bb9
juniorsolo <junior.solo@hotmail.com>
2017-02-14 21:24:02 -0300

    Pegando campos digitados e incluindo na table, limapndo campos em seguida.

--- adiciona.js
+++ adiciona.js
@@ -4,17 +4,28 @@ botaoAdd.addEventListener("click", function(event){
 
 	event.preventDefault();
 
-	var pacienteNovo = "<tr class='paciente'><td class='info-nome'>Rafael</td>"+
-						"<td class='info-peso'>90</td><td class='info-altura'>1.65</td>"+
+	var campoNome = document.querySelector("#campo-nome");
+	var campoPeso = document.querySelector("#campo-peso");
+	var campoAltura = document.querySelector("#campo-altura");
+
+
+	var pacienteNovo = "<tr class='paciente'>"+
+						"<td class='info-nome'>" + campoNome.value + "</td>"+
+						"<td class='info-peso'>" + campoPeso.value + "</td>"+
+						"<td class='info-altura'>" + campoAltura.value + "</td>"+
 						"<td class='info-imc'></td></tr>";
 
 	// var tabela = document.getElementsByTagName("table")[0];
 
-	// retorna um array
-	//document.querySelectorAll("table");
+	//document.querySelectorAll("table"); retorna um array
 
 	//sempre pega o primeiro elemento de varios se houver
 	var tabela  = document.querySelector("table");
 	tabela.innerHTML = tabela.innerHTML + pacienteNovo;
+    
+    //Limpa campos
 
+    campoNome.value = "";
+    campoPeso.value = "";
+    campoAltura.value = "";
 })
